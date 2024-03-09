79d7170d83b2145bfbba517c3025a7242e6d30c9
Nolan Anderson <120975081+nolanranderson@users.noreply.github.com>
2024-03-08 21:50:24 -0800

    Use docker to run compilation for specified compilers and versions

--- compilation/compile_server.py
+++ compilation/compile_server.py
@@ -27,6 +27,8 @@ import sys
 import json
 import hashlib
 import os
+import s3_upload
+import docker
 
 ##############################################################################
 # App Creation
@@ -47,6 +49,10 @@ def post_data(compiler: str, version:str):
     data = request.get_data()
     data_str = data.decode('utf-8')
 
+    # check to see if we have any data, bad request if nothing is provided
+    if (len(data_str) == 0):
+        return jsonify("No source code provided."), 400
+
     # find extension from compiler
     cmp_to_ext = {"gcc": ".c", "clang": ".c", "clang++": ".cpp", "g++": ".cpp"}
 
@@ -56,34 +62,63 @@ def post_data(compiler: str, version:str):
     header = compiler + version
     sha256_hash = hashlib.sha256((data_str + header).encode()).hexdigest()
 
-    # save string as c file
+    # save string as c/cpp file
     file_name = sha256_hash + extension
     with open(file_name, 'w', encoding='utf-8') as file:
         file.write(data_str)
 
-    # Find correct container
+    # get docker client
+    client = docker.from_env()
 
-    # copy file to container
-    # docker cp input_file.c container_id:/path/in/container
+    # script path (docker needs absolute references for volume)
+    script_path = sys.path[0]
 
-    # compile code to container
-    # docker exec -it container_id /path/in/container/compile.sh input_file.c
+    volumes = []
+    # mount for getting source file in container
+    volumes.append(script_path + "/" + file_name + ":/file" + extension)
+    # mount compilation folder on host as out folder in container
+    volumes.append(script_path + ":/out")
 
-    result = subprocess.run(['sh','compile.sh', compiler, file_name], capture_output=True)
-    stderr_result = result.stderr.decode('utf-8')  # Standard error as a string
+    # image names are formatted name:tag
+    image_name = compiler.replace("++", "") + ":" + version
+    # get command to run
+    cmd = compiler + " file" + extension + " -o /out/" + sha256_hash
+    
+    try:
+        # run the command on the appropriate container
+        client.containers.run(image_name, cmd, volumes=volumes)
+    except docker.errors.ContainerError as e:
+        # compilation failed (skill issue)
+        # get stderr for compilation error message
+        stderr_result = e.stderr.decode("utf-8")
+        # delete source file
+        os.remove(file_name)
+        return jsonify(stderr_result), 500
+    except docker.errors.ImageNotFound as e:
+        # missing image
+        os.remove(file_name)
+        return jsonify("Docker: Image for the selected compiler and version is missing."), 500
+    except Exception as e:
+        # some other error
+        os.remove(file_name)
+        return jsonify("Docker: " + str(e) + "."), 500
 
-    # after compilation delete the file from local storage
-    # os.remove(file_name)
+    # after compilation delete the source from local storage
+    os.remove(file_name)
 
-    if result.returncode == 0:
-        return jsonify({'message': f'Received file: {data_str}'}), 200
-    else:
-        substring = "undefined"
-        start_index = stderr_result.find("error")
-        end_index = stderr_result.find("generated")
-        if start_index != -1 and end_index != -1:
-            substring = stderr_result[start_index:end_index + len("generated")]
-        return jsonify(substring), 400
+    # upload the compiled executable to s3 bucket
+    try:
+        s3_upload.upload(sha256_hash)
+    except Exception as e:
+        # some error
+        # delete executable file
+        os.remove(sha256_hash)
+        return jsonify("S3: " + str(e) + "."), 500
+
+    # after upload remove executable from local storage
+    os.remove(sha256_hash)
+
+    return jsonify({'message': f'Received file: {data_str}'}), 200
 
 
 ##############################################################################
@@ -93,4 +128,9 @@ if __name__ == '__main__':
     # set IPV4 as an environment variable..
     # export IPV4="YOUR_IP_ADDR"
     # ip_addr = os.getenv("IPV4")
+
+    # set script directory as current directory
+    os.chdir(sys.path[0])
+
+    # start app
     app.run(host="0.0.0.0", port=9000)
