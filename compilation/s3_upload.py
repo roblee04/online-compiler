22763bcda2e1f573b677eceeb4d0cb915211a77b
Nolan Anderson <120975081+nolanranderson@users.noreply.github.com>
2024-03-08 21:43:02 -0800

    Edit upload function to allow for calling from compile_server.py

--- compilation/s3_upload.py
+++ compilation/s3_upload.py
@@ -18,29 +18,22 @@ from botocore.exceptions import ClientError
 
 ##############################################################################
 # helper to upload to s3
-def upload_to_s3(s3, file_name, bucket):
+def upload(file_name):
+    s3 = boto3.client('s3')
+    bucket = "s3-id3-demo"
     
     object_name = os.path.basename(file_name)
 
     try:
         s3.upload_file(file_name, bucket, object_name)
-
     except ClientError as e:
         logging.error(e)
         return jsonify({'error': f'{e}'}), 400
     return jsonify({'success': True}), 200
 
-
-##############################################################################
-# init variables
-def main():
-    s3 = boto3.client('s3')
-    file_name = sys.argv[1]
-    bucket = 's3-ide-demo'
-    upload_to_s3(s3, file_name, bucket)
-
     
 ##############################################################################
-# call main
+# upload file specified in arg
 if __name__ == '__main__':
-    main()
+    file_name = sys.argv[1]
+    upload(file_name)
